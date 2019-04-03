96f536f14d8362e8afb389cb84dbbba21b14f189
dherrada <dylan.herrada@gmail.com>
2019-04-03 09:28:57 -0400

    Changed runner.py to use folders. Moved question1 and question2 to those folders.

--- runner.py
+++ runner.py
@@ -1,17 +1,20 @@
-# Opens file. File get's printed. ___ gets treated as input field. input gets written to file at specific location. code gets tested.
+# Opens file. File get's printed. ___ gets treated as input field.
+# Input gets written to file at specific location. code gets tested.
+
 from doctester import tester
 
-paths = [['question1', {-2: -1, 15: 16}, 'addone'], 
-        ['question2', {3: 2, -15: -16}, 'subtractone']]
+paths = [['question1', {-2: -1, 15: 16}, 'addone'],
+         ['question2', {3: 2, -15: -16}, 'subtractone']]
+
 
 def runner(paths):
     filename, extra_tests, function_name = paths
-    QuestionF = open(filename+'.py', 'r')
-    AnswerF = open(filename+'_answer.py', 'w')
+    QuestionF = open('questions/'+filename+'.py', 'r')
+    AnswerF = open('answers/'+filename+'_answer.py', 'w')
 
     QStr = QuestionF.read()
     QList = QStr.split('___')
-    print(QList[0], end = "")
+    print(QList[0], end="")
     answer = input()
     print(QList[1])
 
@@ -21,8 +24,9 @@ def runner(paths):
     QuestionF.close()
     AnswerF.close()
 
-    tests = tester([filename+'_answer', extra_tests, function_name])
+    tests = tester(['answers.'+filename+'_answer', extra_tests, function_name])
     tests.test()
+
 
 for path in paths:
     runner(path)
