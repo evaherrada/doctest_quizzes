efd256a00ee6137b3ed468f80193f5dddf1895c1
dherrada <dylan.herrada@gmail.com>
2019-04-03 13:18:27 -0400

    runner.py is now a class, you are now able to run a selection of questions as a quiz.

--- runner.py
+++ runner.py
@@ -1,32 +1,39 @@
 # Opens file. File get's printed. ___ gets treated as input field.
 # Input gets written to file at specific location. code gets tested.
 
-from doctester import tester
+#from doctester import tester
 
+quiz = 'quiz1'
 paths = [['question1', {-2: -1, 15: 16}, 'addone'],
          ['question2', {3: 2, -15: -16}, 'subtractone']]
 
+class runner:
+    def __init__(self, quiz):
+        self.quiz = quiz
+        self.importlib = __import__('importlib')
+        self.tester = self.importlib.import_module('doctester')
 
-def runner(paths):
-    filename, extra_tests, function_name = paths
-    QuestionF = open('questions/'+filename+'.py', 'r')
-    AnswerF = open('answers/'+filename+'_answer.py', 'w')
+    def run(self, paths):
+        filename, extra_tests, function_name = paths
+        QuestionF = open('quizzes/'+self.quiz+'/'+filename+'.py', 'r')
+        AnswerF = open('answers/'+self.quiz+'/'+filename+'_answer.py', 'w')
 
-    QStr = QuestionF.read()
-    QList = QStr.split('___')
-    print(str(QList[0])[:-4], end="")
-    answer = input('\t')
-    print(QList[1])
+        QStr = QuestionF.read()
+        QList = QStr.split('___')
+        print(str(QList[0])[:-4], end="")
+        answer = input('    ')
+        print(QList[1])
 
-    AStr = QList[0] + answer + QList[1]
-    AnswerF.write(AStr)
+        AStr = QList[0] + answer + QList[1]
+        AnswerF.write(AStr)
 
-    QuestionF.close()
-    AnswerF.close()
+        QuestionF.close()
+        AnswerF.close()
 
-    tests = tester(['answers.'+filename+'_answer', extra_tests, function_name])
-    tests.test()
+        tests = self.tester.tester(['answers.'+self.quiz+'.'+filename+'_answer', extra_tests, function_name])
+        tests.test()
 
 
+run = runner(quiz)
 for path in paths:
-    runner(path)
+    run.run(path)
