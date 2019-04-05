5677fcf771f0a5652a5fa7752170211866e23222
dherrada <dylan.herrada@gmail.com>
2019-04-04 20:19:43 -0400

    Moved both classes to the same file

--- doctester.py
+++ doctester.py
@@ -1,15 +1,16 @@
+import doctest
+import importlib
+
 class tester:
     def __init__(self, everything):
         self.filename, self.extra_tests, self.function_name = everything
-        self.importlib = __import__('importlib')
-        self.doctest = __import__('doctest')
 
     def test(self):
-        module = self.importlib.import_module(self.filename)
+        module = importlib.import_module(self.filename)
         self.function = getattr(module, self.function_name)
         globs = {self.function.__name__: self.function}  # globals
         self.new_tests = tester.format_tests(self)
-        self.doctest.run_docstring_examples(
+        doctest.run_docstring_examples(
             self.function.__doc__ + self.new_tests,
             globs,
             name=self.function.__name__,
@@ -25,3 +26,45 @@ class tester:
             else:
                 new_tests += "\t{}\n".format(self.extra_tests[keys])
         return new_tests
+
+
+class runner:
+    def __init__(self, quiz):
+        self.quiz = quiz
+
+    def run(self, paths):
+        input("Press enter to start/continue")
+        filename, extra_tests, function_name = paths
+        QuestionF = open('quizzes/'+self.quiz+'/'+filename+'.py', 'r')
+        AnswerF = open('answers/'+self.quiz+'/'+filename+'_answer.py', 'w')
+
+        QStr = QuestionF.read()
+        QList = QStr.split('___')
+        print(str(QList[0])[:-4], end="")
+        
+        AnswerF.write(QList[0][:-4])
+
+        while True:
+            try:
+                answer = input('    ')
+            
+                AnswerF.write('    ' + answer + '\n')
+        
+            except EOFError:
+                break
+
+        QuestionF.close()
+        AnswerF.close()
+
+        tests = tester(['answers.'+self.quiz+'.'+filename + '_answer', 
+                        extra_tests, function_name])
+        tests.test()
+
+
+quiz = 'quiz1'
+paths = [['question1', {-2: -1, 15: 16}, 'addone'],
+         ['question2', {3: 2, -15: -16}, 'subtractone']]
+
+run = runner(quiz)
+for path in paths:
+    run.run(path)
